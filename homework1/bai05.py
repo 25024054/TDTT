552f859333e03162af16635f82a5b4e8c356b8c7
25024054 <25024054@vnu.edu.vn>
2026-03-13 08:28:29 +0700

    bài 5 và bài 7P2

---
+++ homework1/bai05.py
@@ -0,0 +1,5 @@
+a=input("Nhập chữ cái in hoa :")
+if a=="A":
+ print("z")
+else:
+ print(chr(ord(a)+32-1)) 
---
+++ homework1/bai7P2.py
@@ -0,0 +1,9 @@
+a=int(input("Nhập điểm :"))
+if a >=8:
+ print("Giỏi")
+elif a >=6.5 and a <8:
+ print("Khá")
+elif a >=5 and a <6.5:
+ print("Trung bình")
+elif a <5:
+ print("Yếu")
